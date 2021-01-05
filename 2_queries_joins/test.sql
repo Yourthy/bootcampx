cf581565771a5853c8816d4ed2fea4a332497989
Yourthy <christopher.t.yourth@gmail.com>
2021-01-05 06:46:47 +0000

    completed 3rd set of querie problems using HAVING AND GROUP BY FUNCTIONS

--- 2_queries_joins/test.sql
+++ 2_queries_joins/test.sql
@@ -1,13 +1,11 @@
-SELECT SUM(assignment_submissions.duration)
-FROM students
-JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+-- SELECT SUM(assignment_submissions.duration)
+-- FROM students
+-- JOIN assignment_submissions ON students.id = assignment_submissions.student_id
 
-WHERE students.name = 'Ibrahim Schimmel';
+-- WHERE students.name = 'Ibrahim Schimmel';
 
-SELECT sum(assignment_submissions.duration) AS total_duration
-FROM cohorts
-JOIN students ON cohorts.id = students.cohort_id
-JOIN assignment_submissions ON students.id = assignment_submissions.student_id
-WHERE cohorts.name = 'FEB12';
-
-
+-- SELECT sum(assignment_submissions.duration) AS total_duration
+-- FROM cohorts
+-- JOIN students ON cohorts.id = students.cohort_id
+-- JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+-- WHERE cohorts.name = 'FEB12';
