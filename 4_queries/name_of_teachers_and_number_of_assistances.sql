96399914d108edbf780eb2aeb9566b7b8865d9a2
Yourthy <christopher.t.yourth@gmail.com>
2021-01-05 23:06:09 +0000

    completed new set of query activities stored in 4_queries

---
+++ 4_queries/name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher, cohorts.name AS name, count(assistance_requests) AS total_instances
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+GROUP BY teachers.name, cohorts.name
+HAVING cohorts.name = 'JUL02'
+ORDER BY teachers.name;
