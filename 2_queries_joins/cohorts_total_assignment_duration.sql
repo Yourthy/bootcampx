1023e585428983b6ebcf1abfa3c9239c6a609e81
Yourthy <christopher.t.yourth@gmail.com>
2021-01-05 05:36:29 +0000

    created a test.sql file and tried to write out the problem in another way to make sure I understood the JOIN concepts

--- 2_queries_joins/cohorts_total_assignment_duration.sql
+++ 2_queries_joins/cohorts_total_assignment_duration.sql
@@ -1,5 +1,5 @@
-SELECT sum(assignment_submissions.duration) as total_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
+SELECT sum(assignment_submissions.duration) AS total_duration
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
 WHERE cohorts.name = 'FEB12';
--- 2_queries_joins/total_amount_time_student_spent_all_assignments.sql
+++ 2_queries_joins/total_amount_time_student_spent_all_assignments.sql
@@ -1,5 +1,4 @@
-SELECT SUM(assignment_submissions.duration) AS total_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
+SELECT SUM(assignment_submissions.duration)
+FROM students
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
 WHERE students.name = 'Ibrahim Schimmel';
-
